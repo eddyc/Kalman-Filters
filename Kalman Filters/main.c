b6a4a31234a33a3b99e1c46b42cd7903dc7c5481
Edward Costello <edwardcostello@gmail.com>
2013-06-24 17:26:15 +0200

    Initial Commit

---
+++ Kalman Filters/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Kalman Filters
+//
+//  Created by Edward Costello on 24/06/2013.
+//  Copyright (c) 2013 Edward Costello. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
